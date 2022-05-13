19aa41b8fe45c9d76ec4bba8e1cfd21bb0d964aa
Ouriel91 <orieloh91@gmail.com>
2022-05-13 17:32:48 +0300

    feat update: adds main logic of game

--- app.js
+++ app.js
@@ -15,3 +15,114 @@ const endGameData = [{
 const stones = document.querySelectorAll('.stone');
 const avengers = document.querySelectorAll('.avenger');
 const glove = document.querySelector('.infinity_glove');
+
+const objCheck = {name: "", avengers:[]}
+
+glove.addEventListener('click', () => {
+    endGameData.forEach((item) => {
+
+        const avengersArr = objCheck.avengers
+        const itemsArr = item.avengers
+        //lexical sort
+        avengersArr.sort()
+        itemsArr.sort()
+
+        if(avengersArr.length !== itemsArr.length){
+            return
+        }
+
+        if(objCheck.name === item.name){
+
+            if(objCheck.name === "time-stone"){
+
+                if(avengersArr.length === 1 && avengersArr[0] === item.avengers[0].name){
+                    cleanUpUiAfterAnswer(objCheck.name, avengersArr, true)
+                    winnigCheck()
+                    return
+                }
+
+                //what if not
+            }
+            
+            for(let i = 0; i < avengersArr.length; i++){
+                if((avengersArr[i]) !== itemsArr[i]){
+                    alert("wrong answer")
+                    cleanUpUiAfterAnswer(objCheck.name, avengersArr, false)
+                    return
+                }
+            }
+
+            cleanUpUiAfterAnswer(objCheck.name, avengersArr, true)
+            winnigCheck()
+        }
+    })
+})
+
+function cleanUpUiAfterAnswer(name, avengers, answer) {
+
+    if(!answer){ 
+        document.getElementById(name).style.backgroundColor = "transparent"
+    }
+    else{
+        document.getElementById(name).style.display = "none"
+    } 
+
+    avengers.forEach((avenger) => {
+        document.getElementById(avenger).style.backgroundColor = "transparent"
+    }) 
+
+    prepareForReCheck()
+}
+
+function prepareForReCheck() {
+    objCheck.name = ""
+    objCheck.avengers = []
+}
+
+function winnigCheck() {
+    
+    const displayStones = document.querySelectorAll(".stone")
+    for (let i = 0; i < displayStones.length; i++) {
+        if (!(displayStones[i].style.display === "none")){
+            console.log("not win")
+            return
+        }
+    }
+
+    console.log("win!!!")
+
+    alert("you win")
+    displayStones.forEach(stone => {
+        stone.style.display = "block"
+        stone.style.backgroundColor = "transparent"
+    })
+}
+
+function changeBackgroundItem(item) {
+    item.style.backgroundColor = 'blue'
+}
+
+//need some another work for user experience
+function storeData (item) {
+
+    if(item.includes('stone')){
+        objCheck.name = item
+    }
+    else {
+        objCheck.avengers.push(item)
+    }  
+}
+
+stones.forEach(stone => {
+    stone.addEventListener('click', () => {
+        changeBackgroundItem(stone)
+        storeData(stone.id)
+    })
+})
+
+avengers.forEach(avenger => {
+    avenger.addEventListener('click', () => {
+        storeData(avenger.id)
+        changeBackgroundItem(avenger)
+    })
+})
